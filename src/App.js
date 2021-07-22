dc9dcee95344fc1d36bcc23f6500cdb5140bdf3b
Rob Blackbourn <rob.blackbourn@gmail.com>
2021-07-22 12:59:39 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Matrix from './components/Matrix'
+
+function App() {
+  return (
+    <div>
+      <Matrix />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Matrix.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Matrix extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Test</h1>
+                <p>
+                    This is not a test.
+                </p>
+            </div>
+        )
+    }
+}
